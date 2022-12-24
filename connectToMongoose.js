c7db8bb7c120a0413d19eace965ff02c7d51dc67
olamarvel <olatundemarvelousanthony@gmailcom>
2022-12-24 16:41:44 +0100

    cahange database to user instead of bot

--- connectToMongoose.js
+++ connectToMongoose.js
@@ -12,8 +12,7 @@ async function connectToMongoose() {
 
   // Bind connection to error event (to get notification of connection errors)
   db.on('error', console.error.bind(console, 'MongoDB connection error:'))
-
-  return Mongoose.model('bot', userSchema)
+  return Mongoose.model('user', userSchema)
  } catch (e) {
   console.log('handshaking failed')
   console.error(e)
